7ecd258b97afbe0ce6cb54e2ec5ba82e89209891
Moenator <suzukimoto99@gmail.com>
2018-06-29 15:43:03 +0800

    Heymen

--- galewarning.js
+++ galewarning.js
@@ -1,10 +1,11 @@
 var events = [
-  {'Date': new Date(2018, 5, 07), 'Title': 'Preparedness Measures and Effects of Southwest Monsoon Enhanced by Typhoon Domeng', 'Link': function(){$('#modalxDomeng').modal('show');}},
-  {'Date': new Date(2018, 5, 29), 'Title': 'Preparedness and Measures of Tropical Storm "BASYANG" (I.N. SANBA) ', 'Link': function(){$('#modalBasyang').modal('show');}},
-  {'Date': new Date(2018, 0, 15), 'Title': 'Mayon Volcano Phreatic Eruption ', 'Link': function(){$('#modalMayon').modal('show');}},
-  {'Date': new Date(2018, 0, 13), 'Title': 'Effects of Tail-End of a Cold Front (TECF) and Trough of Low Pressure Area ', 'Link': 'http://ndrrmc.gov.ph/8-ndrrmc-update/3292-situational-report-re-effects-of-tail-end-of-a-cold-front-tecf-in-regions-v-and-vi'},
-  {'Date': new Date(2018, 0, 10), 'Title': 'Black Nazarene Traslacion 2018 ', 'Link': 'http://ndrrmc.gov.ph/8-ndrrmc-update/3289-situational-report-re-black-nazarene-traslacion-2018'},
-  {'Date': new Date(2018, 5, 08), 'Title': '', 'Link': ''},
+  {'Date': new Date(2018, 5, 29), 'Title': 'Preparedness Measures and Effects of Southwest Monsoon Enhanced by Typhoon Domeng', 'Link': function(){$('#modalxDomeng').modal('show');}},
+  {'Date': new Date(2018, 5, 28), 'Title': 'Preparedness and Measures of Tropical Storm "BASYANG" (I.N. SANBA) ', 'Link': function(){$('#modalBasyang').modal('show');}},
+  {'Date': new Date(2018, 5, 28), 'Title': 'Hey hey', 'Link': function(){$('#modalBasyang').modal('show');}},
+  {'Date': new Date(2018, 0, 27), 'Title': 'Mayon Volcano Phreatic Eruption ', 'Link': function(){$('#modalMayon').modal('show');}},
+  {'Date': new Date(2018, 0, 26), 'Title': 'Effects of Tail-End of a Cold Front (TECF) and Trough of Low Pressure Area ', 'Link': 'http://ndrrmc.gov.ph/8-ndrrmc-update/3292-situational-report-re-effects-of-tail-end-of-a-cold-front-tecf-in-regions-v-and-vi'},
+  {'Date': new Date(2018, 0, 25), 'Title': 'Black Nazarene Traslacion 2018 ', 'Link': 'http://ndrrmc.gov.ph/8-ndrrmc-update/3289-situational-report-re-black-nazarene-traslacion-2018'},
+  {'Date': new Date(2018, 5, 24), 'Title': '', 'Link': ''},
 
 ];
 var settings = {};
